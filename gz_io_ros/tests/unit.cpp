df88400ad7dd1419f2d279e137c6b64c4b1ceef4
Jordan Krasan <jkrasan@umich.edu>
2023-03-17 20:09:03 -0400

    Fixed tests

--- gz_io_ros/tests/unit.cpp
+++ gz_io_ros/tests/unit.cpp
@@ -1,5 +1,7 @@
 #include <gtest/gtest.h>
 
+#include <geometry_msgs/msg/twist.hpp>
+#include <nav_msgs/msg/odometry.hpp>
 #include <rclcpp/rclcpp.hpp>
 
 #include "gz_io_ros/gz_io_ros.hpp"
@@ -9,15 +11,16 @@ TEST(gz_io_ros, MsgConvertTest) {
     rclcpp::NodeOptions opts;
     gir::GzIoRos node{opts};
 
-    geometry_msgs::msg::Twist::SharedPtr t{};
-    nav_msgs::msg::Odometry::SharedPtr o{};
-    t->linear.x = 5.0;
-    t->angular.z = 1.57;
-    o->twist.twist.linear.x = 2.5;
-    ackermann_msgs::msg::AckermannDrive msg = node.convert_data(o, t);
-    ASSERT_FLOAT_EQ(msg.acceleration, t->linear.x) << "Acceleration must be equal to twist's linear x!";
-    ASSERT_FLOAT_EQ(msg.speed, o->twist.twist.linear.x) << "Speed must be equal to odom's linear x!";
-    ASSERT_FLOAT_EQ(msg.jerk, 0.0) << "Jerk must be equal to zero!";
+    geometry_msgs::msg::Twist t{};
+    nav_msgs::msg::Odometry o{};
+    t.linear.x = 5.0;
+    t.angular.z = 1.57;
+    o.twist.twist.linear.x = 2.5;
+    ackermann_msgs::msg::AckermannDrive msg =
+        node.convert_data(std::make_shared<nav_msgs::msg::Odometry>(o), std::make_shared<geometry_msgs::msg::Twist>(t));
+    EXPECT_FLOAT_EQ(msg.acceleration, t.linear.x) << "Acceleration must be equal to twist's linear x!";
+    EXPECT_FLOAT_EQ(msg.speed, o.twist.twist.linear.x) << "Speed must be equal to odom's linear x!";
+    EXPECT_FLOAT_EQ(msg.jerk, 0.0) << "Jerk must be equal to zero!";
 }
 
 int main(int argc, char** argv) {
