d2d96ee607a2fe859e1150348a4f863d30bb5838
MarkPopkov <112526667+MarkPopkov@users.noreply.github.com>
2026-03-26 12:38:57 -0400

    Fixed PID values
    Set the PID values to a more typical range after removing the multiplier in the previous commit. NOT FULLY TUNED (yet)
    
    Signed-off-by: MarkPopkov <112526667+MarkPopkov@users.noreply.github.com>

--- phnx_io_ros/include/phnx_io_ros/pid_interface.hpp
+++ phnx_io_ros/include/phnx_io_ros/pid_interface.hpp
@@ -11,7 +11,7 @@
 /// Threadsafe wrapper around PID
 class PidInterface {
     /// Controller
-    phnx_control::SpeedController pid{0.8, 0.08, 0.008};
+    phnx_control::SpeedController pid{1.3, 1.2, 0.13};
     /// Control thread
     std::thread thread;
     /// Odom queue
